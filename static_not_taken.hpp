076dc8d7c621349c6603f0c00d3f7d5907597a45
LucasBarbosa0123 <134572773+LucasBarbosa0123@users.noreply.github.com>
2025-07-06 11:37:34 -0400

    Envio das classes de Taken e notTaken

---
+++ static_not_taken.hpp
@@ -0,0 +1,11 @@
+class static_not_taken
+{
+public:
+    static_not_taken();
+    bool predict();
+    void update_state(bool hit);
+    float get_predictor_hit_rate();
+
+private:
+    int Npredictions, Nhits;
+};
